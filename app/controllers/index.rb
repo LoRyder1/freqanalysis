4c3b37181dd84c4e35b50f76aa57e505220b376a
Lovinder Pnag <lspnag@gmail.com>
2015-01-22 20:17:34 -0600

    stripped ed, ing, and s from words

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -22,5 +22,27 @@ get '/' do
   	@drama << x.gsub(/[^a-z]/i, "")
   end
 
+  test1 = "talk, talks, talked, talking"
+
+  prc = test1.downcase.split(" ")
+  @yep =[]
+
+  prc.each do |x|
+  	@yep << x.gsub(/[^a-z]|ed|ing/i, "")
+  end
+
+  @crazy = []
+  @yep.each do |x|
+  	if x.chars.last == "s"
+  		@crazy << x.chomp("s")
+  	else
+  		@crazy << x
+  	end
+  end
+
+  raise @crazy.inspect
+
+  raise @yep.inspect
+
   erb :analysis
 end
