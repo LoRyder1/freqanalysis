2f6f7dcb31b71e994bd8cdc522b04a38ff532a7a
Lovinder Pnag <lspnag@gmail.com>
2015-01-22 18:13:19 -0600

    filtering out commas, apostrophes, and periods from paragraphs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,26 @@
 get '/' do
   # Look in app/views/index.erb
   # erb :index
+
+  @doc = Docx::Document.open('example.docx')
+
+  @paragraphs = []
+  @doc.paragraphs.each do |p|
+  	@paragraphs << p
+  end
+
+  @first = @paragraphs[8].text
+  @test = @first.gsub(/[^a-z]/i, "")
+
+  # raise @test.inspect
+
+  @yo = @first.downcase.split(" ")
+
+  @drama = []
+
+  @yo.each do |x|
+  	@drama << x.gsub(/[^a-z]/i, "")
+  end
+
   erb :analysis
 end
